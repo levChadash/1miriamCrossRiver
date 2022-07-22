d9cbc56dd5f3d6fa6a28db7c98819aeff308bfff
מרים <miriamkaplan123@gmail.com>
2022-07-22 03:13:14 +0300

    test

--- Src/CoronaApp.Application/Program.cs
+++ Src/CoronaApp.Application/Program.cs
@@ -1,36 +1,3 @@
-/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-
-namespace CoronaApp.Api
-{
-    
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            IConfigurationRoot configuration = new
-            ConfigurationBuilder().AddJsonFile("appsettings.json",
-            optional: false, reloadOnChange: true).Build();
-            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration
-            (configuration).CreateLogger();
-            BuildWebHost(args).Run();
-        }
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseSerilog()
-                .Build();
-    }
-}
-*/
 using CoronaApp.Api.Middlewares;
 using CoronaApp.Dal;
 using CoronaApp.Services;
