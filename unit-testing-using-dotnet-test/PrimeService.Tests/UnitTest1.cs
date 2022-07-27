7466b0b01716013cf7020295ab049cce54ba8777
מרים <miriamkaplan123@gmail.com>
2022-07-27 18:06:23 +0300

    ?

--- unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
@@ -2,9 +2,14 @@ namespace PrimeService.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
     {
+        var result = _primeService.IsPrime(value);
 
+        Assert.False(result, $"{value} should not be prime");
     }
 }
