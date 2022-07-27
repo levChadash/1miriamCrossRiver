1665d1e198ca99996b70246dda2886c33eb8344d
מרים <miriamkaplan123@gmail.com>
2022-07-27 18:06:53 +0300

    token

--- Src/CoronaApp.Dal/Mock.cs
+++ Src/CoronaApp.Dal/Mock.cs
@@ -4,14 +4,23 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoronaApp.Dal
+namespace CoronaApp.Dal;
+
+ public class Mock : IDalLocation
 {
-     public class Mock : IDalLocation
+    
+    Patient p1 = new Patient() { Id = "324103357", Name = "Miriam", Age = 9 };
+    Patient p2 = new Patient() { Id = "324864800", Name = "Leah", Age = 9 };
+    Patient p3 = new Patient() { Id = "212825376", Name = "Shani", Age = 9 };
+    Location l1;
+    List<Patient> pl;
+    List<Location> lc;
+
+
+    public Mock()
     {
-        
-        Patient p1 = new Patient() { Id = "324103357", Name = "Miriam", Age = 9 };
-        Patient p2 = new Patient() { Id = "324864800", Name = "Leah", Age = 9 };
-        Patient p3 = new Patient() { Id = "212825376", Name = "Shani", Age = 9 };
+        List<Patient> pl = new List<Patient>();
+        List<Location> lc = new List<Location>();
         Location l1 = new Location()
         {
             LocaionId = 1,
@@ -22,50 +31,43 @@ namespace CoronaApp.Dal
             EndDate = new DateTime(),
             Patient = new Patient() { Id = "324103357", Name = "Miriam", Age = 9 },
         };
-        List<Patient> pl = new List<Patient>();
-        List<Location> lc = new List<Location>();
+        pl.Add(p1);
+        pl.Add(p2);
+        pl.Add(p3);
+        lc.Add(l1);
+    }
+    public Task Delete(Location l)
+    {
+        throw new NotImplementedException();
+    }
 
+    public async  Task<List<Location>> Get()
+    {
+        return lc;
+    }
 
-        public Mock()
-        {
-            pl.Add(p1);
-            pl.Add(p2);
-            pl.Add(p3);
-            lc.Add(l1);
-        }
-        public Task delete(Location l)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<List<Location>> GetByAge(LocationSearch ls)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async  Task<List<Location>> get()
-        {
-            return lc;
-        }
+    public Task<List<Location>> GetByCity(string city)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<List<Location>> getByAge(LocationSearch ls)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<List<Location>> GetByDate(LocationSearch ls)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<List<Location>> getByCity(string city)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<List<Location>> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<List<Location>> getByDate(LocationSearch ls)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Location>> getById(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task post(Location location)
-        {
-            throw new NotImplementedException();
-        }
+    public Task Post(Location location)
+    {
+        throw new NotImplementedException();
     }
 }
